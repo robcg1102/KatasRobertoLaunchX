08a7e5c66ad453433d415aca7261c969dd49c2d0
robcg1102 <robcg1102@gmail.com>
2022-02-13 01:00:39 -0600

    AddFile: Katas OnBoarding

---
+++ Módulo 10/config.py
@@ -0,0 +1,15 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it 'IsADirectoryError'")
+    except PermissionError:
+        print("Found config.txt but it is a directory, couldn't read it 'PermissionError'")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,9 @@
+# Katas realizadas para Launch X 💻🚀🪐
+
+## Primera misión - OnBoarding 🛸
+
+Esquema de la misión: Programación 
+
+- Katas del módulo 0 al módulo 10
+
+### Por Roberto Carro Gastélum
